30737db522928ea581e60aeb687086a1b9de4ee2
Mitra Electronics <ishanmitra020@gmail.com>
2022-05-25 15:59:34 +0530

    v0.1.0

--- src/py_utils/__create__.py
+++ src/py_utils/__create__.py
@@ -3,17 +3,10 @@ import typer
 from subprocess import run as run_shell
 import os
 
-def __create__(exe):
+def __create__(exe, name, app_type, framework, git_init):
     run_shell('"'+exe+'" -m pip freeze', shell=True)
-    name: str = typer.Option(prompt="Enter the name of the project")
-    if os.path.exists(name):
-        typer.echo(f"A folder with {name} already exists. Please")
-        raise typer.Abort()
-    else:
-        os.mkdir(name)
-        os.mkdir(os.path.join(name, ".app"))
-    
-    
+    os.mkdir(name)
+    os.mkdir(os.path.join(name, ".app"))
 
     with open(os.path.join(name, ".app", "metadata.json"), "w") as f:
         f.write(json.dumps({
--- src/py_utils/__init__.py
+++ src/py_utils/__init__.py
@@ -6,8 +6,11 @@ from py_utils.__create__ import __create__
 app = typer.Typer()
 
 @app.command()
-def create(name: str):
-    __create__(sys.executable, name)
+def create(
+    name: str = typer.Option(..., prompt="Enter the nae of the project"),
+    app_type : str = typer.Option(..., prompt="Enter the app type (web, app, game)")
+    ):
+    __create__(sys.executable)
 
 @app.command()
 def run(script: str):
